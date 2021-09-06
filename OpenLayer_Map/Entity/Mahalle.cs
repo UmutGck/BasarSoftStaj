112f839d2344be2ef2e81d59a8c33f4aac35907e
UmutGck <gucukumut@gmail.com>
2021-09-06 22:12:26 +0300

    ProjeEklendi

---
+++ OpenLayer_Map/Entity/Mahalle.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace OpenLayer_Map.Entity
+{
+    public class Mahalle //umut düzellet
+    {
+        [Key]
+
+        [Display(Name = "Id")]
+        public int Id { get; set; }
+        public string MahalleAdi { get; set; }
+        public string Koordinatlar{ get; set; }  
+
+    }
+}
